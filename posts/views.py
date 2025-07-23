1ed92d2ccf38bdc604334f789db75e369da6775d
Vincent Chiriba <vinlawz07@gmail.com>
2025-07-23 19:15:36 +0300

    feat: Implement post management functionality
    - Add PostForm for post creation and updates
    - Implement PostCreateView, PostUpdateView, and PostDeleteView
    - Add post_confirm_delete.html template
    - Update URLs for post management
    - Add proper authentication and authorization

--- posts/views.py
+++ posts/views.py
@@ -1,32 +1,63 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from .forms import PostForm
 
 class PostListView(ListView):
     model = Post
     template_name = 'posts/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-created_at']
+    paginate_by = 10
 
 class PostDetailView(DetailView):
     model = Post
+    context_object_name = 'post'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content', 'image']
+    form_class = PostForm
     template_name = 'posts/post_form.html'
+    success_message = 'Your post has been created!'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, self.success_message)
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    success_message = 'Your post has been updated!'
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = '/'
+    template_name = 'posts/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+    success_message = 'Your post has been deleted!'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
