e400551f8743fefe88062108dcf7cde55af30df3
Rebecca-254 <gitaurebecca407@gmail.com>
2025-07-28 16:24:42 +0300

    modified urls

--- echoes_of_the_world/urls.py
+++ echoes_of_the_world/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('accounts/', include('accounts.urls')), 
+    path('accounts/', include('accounts.urls')), 
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('posts/', include('posts.urls')), # Custom app for posts
 ]
